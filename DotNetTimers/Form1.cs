7a25337661fa3c4fe7f62bb8426392656f392eb5
codest <3722845+prime167@users.noreply.github.com>
2019-09-30 13:23:08 +0800

    添加项目文件。

---
+++ DotNetTimers/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DotNetTimers
+{
+    public partial class Form1 : Form
+    {
+        private int _count;
+        private readonly Timer _timer = new Timer();
+        private readonly Label _lblCount = new Label();
+        public Form1()
+        {
+            InitializeComponent();
+            Size = new Size(450,450);
+            Controls.Add(_lblCount);
+            _lblCount.Location = new Point(200, 200);
+            _timer.Interval = 100;
+            _timer.Tick += timer1_Tick;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _timer.Enabled = true;
+
+            // 或者
+            //_timer.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            _count++;
+            _lblCount.Text = _count.ToString("0000");
+            if (_count >= 30)
+            {
+                _timer.Enabled = false;
+
+                // 或者
+                //_timer.Stop();
+            }
+        }
+    }
+}
---
+++ DotNetTimers/FormMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotNetTimers
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var form1 = new Form1();
+            form1.ShowDialog(this);
+        }
+    }
+}
